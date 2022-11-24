77bee7dfc2aafe47c3b9069a8d77bba3b4a4bf01
ShynarZhakan <schubblezam@gmail.com>
2022-11-24 15:55:13 +0600

    Commit for 2nd task

---
+++ c#_HW1/2_Task/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введите первое целое число");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе целое число");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+int max = 0;
+
+if (num1 > num2)
+{
+    max = num1;
+}
+
+else 
+{
+    max = num2;
+}
+
+Console.WriteLine($"Максимальное число равно {max} ");
